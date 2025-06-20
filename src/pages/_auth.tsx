d3fa2f723f4b712cb8214ab8138264fdb6109c7a
uriel285 <contactoherrerauriel@gmail.com>
2025-06-20 16:56:47 -0300

    auth fix

--- src/pages/_auth.tsx
+++ src/pages/_auth.tsx
@@ -1,7 +1,20 @@
-if (typeof window !== "undefined") {
-    // Next.js usa este paquete para el overlay
-    // Esto lo deshabilita
-    // @ts-ignore
-    window.__NEXT_REACT_DEVTOOLS_GLOBAL_HOOK__ = null;
-  }
-  
+// pages/_Auth.tsx
+import React, { useEffect } from 'react';
+
+const AuthPage = () => {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      // Deshabilitar React DevTools en producción
+      // @ts-ignore
+      window.__NEXT_REACT_DEVTOOLS_GLOBAL_HOOK__ = null;
+    }
+  }, []);
+
+  return (
+    <div>
+      <h1>Auth Page</h1>
+    </div>
+  );
+};
+
+export default AuthPage;
